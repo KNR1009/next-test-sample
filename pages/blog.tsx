05816b5e21ee4ca3331e4c483580a1f612b7fe93
伊東和成 <itokazunari5375@icloud.com>
2022-07-09 15:52:59 +0900

    apiでデータを取得し表示

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,53 @@
+import axios from "axios";
 import type { NextPage } from "next";
-// components
+import { useEffect, useState } from "react";
+
+// 取得するブログ記事の型
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
 const BlogPage: NextPage = () => {
-  return <p>ブログです</p>;
+  // 取得したブログデータを格納するstate
+  const [postDates, setPostDates] = useState<Post[]>([]);
+  // 外部APIからブログデータを取得
+  const getPosts = async (): Promise<Post[]> => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return response.data;
+  };
+  // レンダリング時にAPIコール関数を実行し取得したデータでstateを更新
+  useEffect(() => {
+    try {
+      const getPostDates = async () => {
+        const result = await getPosts();
+        setPostDates(result);
+      };
+      getPostDates();
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  return (
+    <div>
+      {postDates.length === 0 ? (
+        <p>ローディング中</p>
+      ) : (
+        <>
+          {postDates.map((i) => (
+            <p key={i.id}>
+              {i.id}: {i.body}
+            </p>
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default BlogPage;
